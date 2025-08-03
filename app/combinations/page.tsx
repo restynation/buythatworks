f4d27cf0afb770bc0edc5e54db4f2bb785bc2a54
yongwoo <team@restynation.com>
2025-08-03 14:58:05 +0900

    Reduce device block size to 100px on mobile

--- app/combinations/page.tsx
+++ app/combinations/page.tsx
@@ -773,8 +773,8 @@ function CombinationsPageContent() {
                       const deviceInfo = getDeviceInfo(block)
                       
                       return (
-                        <div key={block.id || index} className="flex flex-col items-center w-[140px] h-[140px]">
-                          <div className="w-[124px] h-[108px] p-3 flex items-center justify-center">
+                        <div key={block.id || index} className="flex flex-col items-center w-[100px] h-[100px] md:w-[140px] md:h-[140px]">
+                                                      <div className="w-[84px] h-[68px] md:w-[124px] md:h-[108px] p-3 flex items-center justify-center">
                             {block.products?.image_url ? (
                               <img 
                                 src={block.products.image_url} 
